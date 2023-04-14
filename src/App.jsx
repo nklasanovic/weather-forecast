546e78f0445552374211e6cd53d7a2e8d848cfaa
nebojsaklasanovic <nebojsa.shone9998@gmail.com>
2023-04-14 14:11:17 +0200

    Project completed

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import Hot from "./assets/images/summer.jpg";
+import Cold from "./assets/images/winter.jpg";
+import Description from "./components/Description";
+import { getWeatherData } from "./data/weatherService";
+
+const App = () => {
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [city, setCity] = useState("London");
+  const [bg, setBg] = useState(Hot);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getWeatherData(city, units);
+      setWeather(data);
+
+      const threshold = units === "metric" ? 15 : 60;
+      if (data.temp < threshold) setBg(Cold);
+      else setBg(Hot);
+    };
+    fetchData();
+  }, [units, city]);
+
+  const handleUnits = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+    const isCelsius = currentUnit === "C";
+    button.innerText = isCelsius ? "°F" : "°C";
+    setUnits(isCelsius ? "metric" : "imperial");
+  };
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
+  return (
+    <div
+      className="w-full h-screen bg-center bg-cover text-white"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="w-full h-screen bg-black/20">
+        {weather && (
+          <div className="sm:max-w-[600px] h-screen m-auto flex flex-col items-center justify-between p-4">
+            <div className="w-full flex items-center justify-center p-4 rounded-lg bg-[var(--secondary-color)]">
+              <input
+                onKeyDown={enterKeyPressed}
+                className="w-[260px] border-2 border-white rounded-lg bg-transparent p-2 font-semibold focus:outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter city name"
+              />
+              <button
+                title="Navigate between metric/imperial"
+                onClick={(e) => handleUnits(e)}
+                className="px-2.5 py-12 border-none rounded-lg text-xl font-semibold hover:cursor-pointer"
+              >
+                F
+              </button>
+            </div>
+
+            <div className="flex items-center rounded-xl justify-between p-4 bg-[var(--secondary-color)]">
+              <div className="flex flex-col items-center justify-center text-center">
+                <h3 className="text-xl font-medium capitalize">{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3 className="text-2xl font-medium capitalize">
+                  {weather.description}
+                </h3>
+              </div>
+              <div className="temperature">
+                <h1 className="text-2xl font-bold">{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+
+            {/* bottom description */}
+            <Description weather={weather} units={units} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Description.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { FaArrowUp, FaArrowDown, FaWind } from "react-icons/fa";
+import { BiHappy } from "react-icons/bi";
+import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
+
+const Description = ({ weather, units }) => {
+  const tempUnit = units === "metric" ? "°C" : "°F";
+  const windUnit = units === "metric" ? "m/s" : "m/h";
+
+  const cards = [
+    {
+      id: 1,
+      icon: <FaArrowDown />,
+      title: "min",
+      data: weather.temp_min.toFixed(),
+      unit: tempUnit,
+    },
+    {
+      id: 2,
+      icon: <FaArrowUp />,
+      title: "max",
+      data: weather.temp_max.toFixed(),
+      unit: tempUnit,
+    },
+    {
+      id: 3,
+      icon: <BiHappy />,
+      title: "feels like",
+      data: weather.feels_like.toFixed(),
+      unit: tempUnit,
+    },
+    {
+      id: 4,
+      icon: <MdCompress />,
+      title: "pressure",
+      data: weather.pressure,
+      unit: "hPa",
+    },
+    {
+      id: 5,
+      icon: <MdOutlineWaterDrop />,
+      title: "humidity",
+      data: weather.humidity,
+      unit: "%",
+    },
+    {
+      id: 6,
+      icon: <FaWind />,
+      title: "wind speed",
+      data: weather.speed.toFixed(),
+      unit: windUnit,
+    },
+  ];
+  return (
+    <div className="grid grid-cols-3 gap-12">
+      {cards.map(({ id, icon, title, data, unit }) => (
+        <div key={id} className="w-[100%] h-[100%] flex flex-col items-center justify-between p-4 text-[16px] rounded-xl bg-[var(--secondary-color)]">
+          <div className="w-full flex flex-row items-center justify-center mb-2">
+            {icon}
+            <small className="ml-2">{title}</small>
+          </div>
+          <h2 className="font-bold">{`${data} ${unit}`}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Description;
